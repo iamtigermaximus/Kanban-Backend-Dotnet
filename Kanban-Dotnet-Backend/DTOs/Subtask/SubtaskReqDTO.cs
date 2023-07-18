1df78cdbc487b9f92a31003c4281653c70790484
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-07-19 02:46:40 +0300

    created Subtask DTOs

---
+++ Kanban-Dotnet-Backend/DTOs/Subtask/SubtaskReqDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Kanban_Dotnet_Backend.DTOs.Subtask;
+
+public class SubtaskReqDTO
+{
+    public int Id { get; set; }
+    public bool Completed { get; set; }
+    public string Text { get; set; }
+}
+
---
+++ Kanban-Dotnet-Backend/DTOs/Subtask/SubtaskResDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Kanban_Dotnet_Backend.Models;
+
+namespace Kanban_Dotnet_Backend.DTOs.Subtask;
+
+public class SubtaskResDTO : BaseModel
+{
+    public int Id { get; set; }
+    public bool Completed { get; set; }
+    public string Text { get; set; }
+}
+
