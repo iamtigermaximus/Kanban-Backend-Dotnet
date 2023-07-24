11b173acc4ffacb5fd025ca4400d655aa394d16b
Siegfred Gamboa <siegy.gamboa@gmail.com>
2023-07-24 05:07:17 +0300

    refactor projectreqdto

--- Kanban-Dotnet-Backend/DTOs/Project/ProjectReqDTO.cs
+++ Kanban-Dotnet-Backend/DTOs/Project/ProjectReqDTO.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using Kanban_Dotnet_Backend.DTOs.Category;
+
 namespace Kanban_Dotnet_Backend.DTOs.Project;
 
 public class ProjectReqDTO
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public List<CategoryReqDTO> Categories { get; set; }
+
 }
 
